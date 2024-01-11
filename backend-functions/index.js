dee1f1009fe1c0c6d0b24cd3d9e5a0d4f1095a6d
RicardoHanna <ricardo.hanna.com@gmail.com>
2024-01-11 11:35:26 +0200

    update

--- backend-functions/index.js
+++
@@ -1,8 +0,0 @@
-// backend-functions/index.js
-
-const functions = require("firebase-functions");
-
-exports.importData = functions.https.onRequest(async (request, response) => {
-  // Your function logic here
-  response.send("Import Data function executed successfully!");
-});
