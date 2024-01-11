ae365491dcf8bd947396f59023c7f71a0fa8e972
RicardoHanna <ricardo.hanna.com@gmail.com>
2024-01-11 14:49:44 +0200

    lo

--- backendserver/functionserver.js
+++ backendserver/functionserver.js
@@ -1,38 +1,33 @@
 const admin = require('firebase-admin');
 const sql = require('mssql');
 
-const serviceAccount = require('./sales-bab47-firebase-adminsdk-gqqph-a89e060434');
+const serviceAccount = require('./sales-bab47-firebase-adminsdk-gqqph-a89e060434.json');
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'https://sales-bab47.firebaseio.com', // Update with your actual database URL
 });
+
 async function importDataToFirestore(userGroupCode) {
-   
   try {
     // Fetch user configuration from Firestore based on user's group code
-// Search for a document where 'userGroupCode' matches the specified value
-const configSnapshot = await admin.firestore().collection('SystemAdmin').where('groupcode', '==', 1).get();
-console.log('Firestore Query:', `groupcode == ${userGroupCode?.toString()}`);
-console.log('Config Snapshot:', configSnapshot.docs.map(doc => doc.data()));
+    const configSnapshot = await admin.firestore().collection('SystemAdmin').where('groupcode', '==', 1).get();
+    console.log('Firestore Query:', `groupcode == ${userGroupCode?.toString()}`);
+    console.log('Config Snapshot:', configSnapshot.docs.map(doc => doc.data()));
 
-if (configSnapshot.empty) {
-  console.error('User configuration not found for group code:', userGroupCode);
-  return;
-}
+    if (configSnapshot.empty) {
+      console.error('User configuration not found for group code:', userGroupCode);
+      return;
+    }
 
-const firstConfigDocument = configSnapshot.docs[0];
-if (!firstConfigDocument.exists) {
-  console.error('Document found, but user configuration not found for group code:', userGroupCode);
-  return;
-}
+    const firstConfigDocument = configSnapshot.docs[0];
+    if (!firstConfigDocument.exists) {
+      console.error('Document found, but user configuration not found for group code:', userGroupCode);
+      return;
+    }
 
-
-const configData = firstConfigDocument.data();
-
-
-// Rest of your code to check and use the configuration data...
+    const configData = firstConfigDocument.data();
 
     if (!configData || !configData.connServer || !configData.connDatabase) {
       console.error('Invalid configuration data:', configData);
